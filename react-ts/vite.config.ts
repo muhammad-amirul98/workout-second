193d908b5230e135b8133b68bd93c636b90173f3
muhammad-amirul98 <moamirul1998@gmail.com>
2025-04-02 10:53:30 +0800

    chore: update config to generate build folder for production

--- react-ts/vite.config.ts
+++ react-ts/vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build", // Ensures the build output goes to the correct folder
+  },
 });
